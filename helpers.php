3840705fe879b2a61ff76f1fb99d02cc159ae814
KhanaliNiyazli1 <niyazlikhanali@gmail.com>
2025-04-16 20:09:55 +0400

    basic route

--- helpers.php
+++ helpers.php
@@ -7,29 +7,55 @@
  * @return string
  */
 
- function basePath($path = ''){
-    return __DIR__ . '/' . $path;
+function basePath($path = ''){
+  return __DIR__ . '/' . $path;
  }
 
- /**
+/**
   * 
   * Load the views
   *
   *@param string $name
   *@return void
+*/
+
+function loadViews($name){
+ $loadView =  basePath("views/{$name}.view.php");
+  
+ if (file_exists($loadView)) {
+    require $loadView;
+} else {
+    echo "View not found";
+  }
+}
+
+/**
+ * Load the partials
+ * 
+ * @param string $name
+ * @return void 
+ */
+
+ function loadPartials($name){
+  $loadPartial = basePath("views/partials/{$name}.php");
+  if (file_exists($loadPartial)) {
+    require $loadPartial;
+  } else {
+    echo "Partial not found";
+}
+ }
+
+
+ /**
+  * 
+  * Inspect values
+  *
+  *@param mixed $value
+  *@return void 
   */
 
-  function loadViews($name){
-   require basePath("views/{$name}.view.php");
-  }
-
-  /**
-   * Load the partials
-   * 
-   * @param string $name
-   * @return void 
-   */
-
-   function loadPartials($name){
-      require basePath("views/partials/{$name}.php");
-   }
+function inspect($value){
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
